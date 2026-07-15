4b49019d039cf1419ed9d12d21a20c2e5613b1d6
maha03-code <mahalakshmia3003@gmail.com>
2026-07-15 16:05:34 +0000

    Complete Day 3 employee grade logic

---
+++ Day03/employee.py
@@ -0,0 +1,16 @@
+Employee_Name = "Mahalakshmi"
+Salary = 50000
+Experience = 5
+Employee_Grade = "A"
+
+
+print("Employee Name:", Employee_Name)
+
+if Experience >= 8:
+    print("Employee Grade: A")
+elif Experience >= 5:
+    print("Employee Grade: B")
+else:
+    print("Employee Grade: C")
+
+
